504a7d3ffa9b19059e7d567acf41116062267439
yapcheahshen <yapcheahshen@gmail.com>
2013-09-23 19:39:38 +0800

    for lecture

--- sample/sample.js
+++ sample/sample.js
@@ -1,16 +1,16 @@
 ﻿console.log(require('yase').build({
 	dbid:'sample',
-	slotshift:7,
-	schema:function() {
-		this.toctag(["sutra","chapter"])
-		      .emptytag("pb").attr("pb","id",{"depth":2})
-		      .attr("chapter","n",{"depth":1,"allowrepeat":true});
-	},  
 	input:'sample.lst',
 	output:'../sample.ydb',
+	slotshift:7,
 	author:'yapcheahshen@gmail.com',
-	version:'0.0.2',
-	outputencoding:'ucs2',
-	estimatesize:100000
-	 //maxfile:0  //
+	schema:function() {
+		this.toctag(["book","sutra","chapter"])
+		      .emptytag("pb").attr("pb","id",{"depth":2})
+		      .attr("chapter","n",{"depth":1,"allowrepeat":true});
+	},
+	TOC:function() {
+		this.addToc("page",[book])
+	}
 }));
+
